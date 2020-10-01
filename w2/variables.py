737397d6a34e623ac3e867a52caf03803f3c4e99
adsoftsito <adsoft@live.com.mx>
2020-10-01 06:14:35 +0000

    add variables.py

---
+++ w2/variables.py
@@ -0,0 +1,17 @@
+# variables
+# Author : Adolfo Centeno
+# Oct 2020
+
+year = 2020
+sentence = "My name is adsoft"
+sarah, bob, mike = 16, 20, 25
+name, age = "mary", 24
+mary = fer = 24
+
+# display values
+print "year = ", year
+print sentence
+print "sarah = ", sarah, "bob =", bob, "mike =", mike
+print "name =", name
+print "age =", age
+print mary, fer
