9448c3a48015086a998bbbcc6eddf4b26f0726e5
adsoftsito <adsoft@live.com.mx>
2020-10-24 14:33:07 +0000

    addd adncomplement.py

--- w5/files.py
+++ w5/files.py
@@ -2,15 +2,16 @@
 # adn example
 adn = ""
 
-f = open("adn.txt", "r")
+f = open("covid19.txt", "r")
 for x in f:
   adn = adn + x
 
-adn = adn.strip()
+adn = adn.replace('\n','')
 
 n = len(adn)
 print " adn length = ", n
+#print adn
 
-for i in range(0, n):
-  print i+1,  adn[i]
+#for i in range(0, n):
+#  print i+1,  adn[i]
 
