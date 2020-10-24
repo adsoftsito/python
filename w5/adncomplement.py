9369ad779b530cd6a84f46a6747605da6cf5e829
adsoftsito <adsoft@live.com.mx>
2020-10-24 15:17:15 +0000

    update codons

--- w5/adncomplement.py
+++ w5/adncomplement.py
@@ -12,11 +12,78 @@ nucl_complement = {
 	      }
 
 rna_transcription = {
-	       "A":"U",
-	       "C":"G",
-               "T":"A",
-               "G":"C"
+	       "A":"A",
+	       "C":"C",
+               "T":"U",
+               "G":"G"
 	      }
+
+codons = {
+  "UUU": "Fenilalanina",
+  "UUC": "Fenilalanina",
+  "UUA": "Leucina",
+  "UUG": "Leucina",
+  "UCU": "Serina",
+  "UCC": "Serina",
+  "UCA": "Serina",
+  "UCG": "Serina",
+  "UAU": "Tirosina",
+  "UAC": "Tirosina",
+  "UAA": "Codon de terminacion ocre",
+  "UAG": "Codon de terminacin Ambar",
+  "UGU": "Cisteina",
+  "UGC": "Cisteina",
+  "UGA": "Codon de terminacion Opalo",
+  "UGG": "Triptofano",
+  "CUU": "Leucina",
+  "CUC": "Leucina",
+  "CUA": "Leucina",
+  "CUG": "Leucina",
+  "CCU": "Prolina",
+  "CCC": "Prolina",
+  "CCA": "Prolina",
+  "CCG": "Prolina",
+  "CAU": "Histidina",
+  "CAC": "Histidina",
+  "CAA": "Glutamina",
+  "CAG": "Glutamina",
+  "CGU": "Arginina",
+  "CGC": "Arginina",
+  "CGA": "Arginina",
+  "CGG": "Arginina",
+  "AUU": "Isoleucina",
+  "AUC": "Isoleucina",
+  "AUA": "Isoleucina",
+  "AUG": "Metionina",
+  "ACU": "Treonina",
+  "ACC": "Treonina",
+  "ACA": "Treonina",
+  "ACG": "Treonina",
+  "AAU": "Asparagina",
+  "AAC": "Asparagina",
+  "AAA": "Lisina",
+  "AAG": "Lisina",
+  "AGU": "Serina",
+  "AGC": "Serina",
+  "AGA": "Arginina",
+  "AGG": "Arginina",
+  "GUU": "Valina",
+  "GUC": "Valina",
+  "GUA": "Valina",
+  "GUG": "Valina",
+  "GCU": "Alanina",
+  "GCC": "Alanina",
+  "GCA": "Alanina",
+  "GCG": "Alanina",
+  "GAU": "acido asartico",
+  "GAC": "acido asartico",
+  "GAA": "acido glutamico",
+  "GAG": "acido glutamico",
+  "GGU": "Glicina",
+  "GGC": "Glicina",
+  "GGA": "Glicina",
+  "GGG": "Glicina"
+}
 
 
 nucl_codon = [];
@@ -41,6 +108,33 @@ print " dna length complementary  = ", n
 for i in range(0, n):
   print i, dna[i], dna_complement[i]
 
-print "dna, dna complementary : "
+print "dna molde : "
 print dna
+
+print "dna complement : "
 print dna_complement
+
+#
+# rna transcription
+#
+
+for x in dna:
+  rna = rna + rna_transcription[x]
+
+print "rna "
+
+rna = rna[::-1]
+
+print rna
+
+#
+# codon create
+#
+n = len(rna)
+for i in range(0, n, 3):
+  nucl_codon.append(rna[i:i+3])
+
+n = len(nucl_codon)
+print "total codons ", n
+for i in range(0,n):
+  print i, nucl_codon[i], codons[nucl_codon[i]]
