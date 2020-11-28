eb539d70c17016d97d7ea6b675a5d07b418a857d
adsoftsito <adsoft@live.com.mx>
2020-11-28 16:36:41 +0000

    add alignIO

---
+++ w10/alignio1.py
@@ -0,0 +1,5 @@
+from Bio import AlignIO
+alignment = AlignIO.read("PF05371_seed.sth", "stockholm")
+print ("Aligment length %i" % alignment.get_alignment_length())
+for record in alignment:
+    print("%s - %s" % (record.seq, record.id))
---
+++ w9/bio_count.py
@@ -0,0 +1,5 @@
+from Bio.Seq import Seq
+my_seq = Seq("GATCGATGGGCCTATATAGGATCGAAAATCGC")
+print (len(my_seq))
+print (my_seq.count("G"))
+print (my_seq.count("C"))
